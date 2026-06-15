7ac704e5ecf0dc90ab888f8b0a4a12b7c1bb4dc7
Stan Gerrits vd Ende <stan.gerritsvandenende@marel.com>
2026-06-15 15:23:56 +0200

    Refactor solution structure by simplifying folder organization in the project file

--- src/Pokedex.Application/Interfaces/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Pokedex.Application.DTOs;
-
-namespace Pokedex.Application.Interfaces;
-
-public interface ITokenService
-{
-    AuthResponseDto GenerateToken(IdentityUser user);
-}
